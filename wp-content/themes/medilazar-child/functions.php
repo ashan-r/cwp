9786eac5d645a1694f2f800ed5c8e6bbc3114ab6
Sandamini Karunarathne <sandamini.karunarathne@qualitapps.com>
2023-11-20 18:43:19 +0530

    Modifications 20112023

--- wp-content/themes/medilazar-child/functions.php
+++ wp-content/themes/medilazar-child/functions.php
@@ -6,6 +6,7 @@
 function cm_child_enqueue_styles_and_scripts()
 {
 	wp_enqueue_style('child-styles', get_stylesheet_directory_uri() . '/style.css', false, time(), 'all');
+	wp_enqueue_style('cm-elementor-styles', get_stylesheet_directory_uri() . '/css/cm-elementor.css', false, time(), 'all');
 	wp_enqueue_script("si_script", get_stylesheet_directory_uri() . "/js/custom.js", '', time());
 }
 add_action('wp_enqueue_scripts', 'cm_child_enqueue_styles_and_scripts', 110000);
